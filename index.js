c01f8f348bdc8ee3c00dcda852695550bfc27ebf
LYDIA <lydiakinuthia91@gmail.com>
2025-03-08 09:40:17 +0300

    first class functions

---
+++ index.js
@@ -0,0 +1,26 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function myCallback() {
+    console.log("Callback function has been called!");
+  }
+  
+  receivesAFunction(myCallback);
+
+  function returnsANamedFunction() {
+    function namedFunction() {
+      console.log("This is a named function!");
+    }
+    return namedFunction;
+  }
+  const myNamedFunction = returnsANamedFunction();
+  myNamedFunction();
+
+  function returnsAnAnonymousFunction() {
+    return function() {
+      console.log("This is an anonymous function!");
+    };
+  }
+  const myAnonymousFunction = returnsAnAnonymousFunction();
+  myAnonymousFunction();
